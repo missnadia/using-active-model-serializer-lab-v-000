bcbb0da7de7da3ea7099c5b2c1ed835fdc23d4c9
Nadia Lee <miss.nadia.lee@gmail.com>
2018-11-05 21:56:04 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,16 +3,6 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-
   def new
     @product = Product.new
   end
@@ -24,10 +14,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
-    end
+    render json: @product, 200
   end
 
   private
