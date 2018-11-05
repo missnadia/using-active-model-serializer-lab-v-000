89443ba8865f2a8912e9c0a3381e982bdd7bda65
Nadia Lee <miss.nadia.lee@gmail.com>
2018-11-05 21:59:21 +0000

    automatically backed up by learn

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe ProductsController, type: :controller do
   describe "GET show" do
     it 'returns a JSON representation of the product' do
       product = Product.create(@product_attributes)
-
       get :show, id: product.id, format: 'json'
       body = JSON.parse(response.body)
       expect(body["name"]).to eq product.name
